f71375fe51e343a13d373f832ad026ae68ebd087
kawa2287 <kawa2287@gmail.com>
2019-09-29 17:16:57 -0500

    initial push

---
+++ app/src/main/java/kawa/chargebinder/Patient.java
@@ -0,0 +1,25 @@
+package kawa.chargebinder;
+
+public class Patient
+{
+    //Variables
+    public int id;
+    public int bed;
+    public String firstName;
+    public String lastName;
+    public int birthYear;
+    public int birthMonth;
+    public int birthDay;
+    public String age;
+    public int admitYear;
+    public int admitMonth;
+    public int admitDay;
+    public String admissionTimeSpan;
+    public String ECPR;
+    public String code;
+    public String CAP;
+    public boolean rtAppropriate;
+    public String ISO;
+
+
+}
